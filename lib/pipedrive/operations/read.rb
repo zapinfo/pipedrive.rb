8dafbf0623def1aef731d189a1720f89faa50389
Non <nontone@gmail.com>
2018-11-29 02:02:16 +0700

    Add find_by_name to operation read

--- lib/pipedrive/operations/read.rb
+++ lib/pipedrive/operations/read.rb
@@ -24,6 +24,15 @@ module Pipedrive
       def find_by_id(id)
         make_api_call(:get, id)
       end
+
+      def find_by_name(*args)
+        params = args.extract_options!
+        params[:term] ||= args[0]
+        raise "term is missing" unless params[:term]
+        params[:search_by_email] ||= args[1] ? 1 : 0
+        return to_enum(:find_by_name, params) unless block_given?
+        follow_pagination(:make_api_call, [:get, "find"], params) { |item| yield item }
+      end
     end
   end
 end
